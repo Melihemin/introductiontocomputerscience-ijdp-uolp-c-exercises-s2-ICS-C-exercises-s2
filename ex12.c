2cec3faf099df3b64459e468ccbf22952c38365a
Ferhat Ucar <67374172+drferhatu@users.noreply.github.com>
2025-11-29 11:38:49 +0300

    Remove factorial calculation code from ex12.c

--- ex12.c
+++ ex12.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
 
-int main() {
-    int n, fact = 1;
-    
-    do {
-        printf("Enter a number (< 50): ");
-        scanf("%d", &n);
-    } while(n >= 50 || n < 0);
-    
-    for(int i = 2; i <= n; i++) {
-        fact *= i;
-    }
-    
-    printf("%d! = %d\n", n, fact);
-    return 0;
-}
