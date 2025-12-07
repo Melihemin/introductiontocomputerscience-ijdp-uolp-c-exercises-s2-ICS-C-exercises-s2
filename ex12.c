90d47c1906f2289dd31cf0f849f2b28e9b4e1b8e
Melih Emin Kılıçoğlu <63915850+Melihemin@users.noreply.github.com>
2025-12-07 23:35:08 +0300

    Implement factorial calculation in ex12.c

--- ex12.c
+++ ex12.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+    int n;
+    unsigned long long fact = 1;
+
+    scanf("%d", &n);
+
+    if (n < 0 || n >= 50) {
+        printf("Invalid input\n");
+        return 0;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    printf("%llu\n", fact);
+
+    return 0;
+}
