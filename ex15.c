ad6151dc63f0c11b823146ead1583f5dee0d2812
Melih Emin Kılıçoğlu <63915850+Melihemin@users.noreply.github.com>
2025-12-07 23:37:10 +0300

    Add diamond pattern printing in ex15.c
    This program prints a diamond pattern using numbers.

--- ex15.c
+++ ex15.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int main() {
+    int i, j, k;
+
+    for (i = 1; i <= 5; i++) {
+        for (j = 5; j > i; j--) {
+            printf(" ");
+        }
+        for (k = 1; k <= i; k++) {
+            printf("%d", k);
+        }
+        for (k = i - 1; k >= 1; k--) {
+            printf("%d", k);
+        }
+        printf("\n");
+    }
+
+    for (i = 4; i >= 1; i--) {
+        for (j = 5; j > i; j--) {
+            printf(" ");
+        }
+        for (k = 1; k <= i; k++) {
+            printf("%d", k);
+        }
+        for (k = i - 1; k >= 1; k--) {
+            printf("%d", k);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
