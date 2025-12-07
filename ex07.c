edb1ed9d5bf8b49aff8249d762191cd75c68a595
Melih Emin Kılıçoğlu <63915850+Melihemin@users.noreply.github.com>
2025-12-07 23:30:21 +0300

    Add loop to print formatted values
    Prints a formatted table of values based on a loop.

--- ex07.c
+++ ex07.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    int i;
+    
+    for (i = 1; i <= 10; i++) {
+        int col1 = i * 5;
+        int col2 = 100 - ((i - 1) * 10 + 1);
+        int col3 = i;
+
+        printf("%d  %d  %d\n", col1, col2, col3);
+    }
+
+    return 0;
+}
