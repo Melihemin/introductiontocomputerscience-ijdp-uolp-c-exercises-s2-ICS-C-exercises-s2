31a39f9a03eac68a80828bb4415bd81c90f9be25
Melih Emin Kılıçoğlu <63915850+Melihemin@users.noreply.github.com>
2025-12-07 23:32:12 +0300

    Implement sum, count, and average calculation

--- ex09.c
+++ ex09.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main() {
+    int num;
+    int count = 0;
+    int sum = 0;
+    float average;
+
+    scanf("%d", &num);
+
+    while (num != -1) {
+        sum += num;
+        count++;
+        scanf("%d", &num);
+    }
+
+    if (count > 0)
+        average = sum / (float)count;
+    else
+        average = 0;
+
+    printf("%d\n", count);
+    printf("%d\n", sum);
+    printf("%.2f\n", average);
+
+    return 0;
+}
